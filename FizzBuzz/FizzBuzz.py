cc8e2be9b38d833d6f0ebd83fdec3d8aee0a830f
Ganeshkumar1508 <65279490+Ganeshkumar1508@users.noreply.github.com>
2021-06-08 17:19:03 +0530

    Add files via upload

---
+++ FizzBuzz/FizzBuzz.py
@@ -0,0 +1,13 @@
+a=input("Enter the range: ")
+a=int(a)
+for fizzbuzz in range(a):
+    if fizzbuzz % 3 == 0 and fizzbuzz % 5 == 0:
+        print("fizzbuzz")
+        continue
+    elif fizzbuzz % 3 == 0:
+        print("fizz")
+        continue
+    elif fizzbuzz % 5 == 0:
+        print("buzz")
+        continue
+    print(fizzbuzz)
